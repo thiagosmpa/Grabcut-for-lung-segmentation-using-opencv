2a4b0d30d4dd4a9943e812590f6e5970dd98a9d9
thiagosmpa <33949962+thiagosmpa@users.noreply.github.com>
2021-03-03 12:41:21 +0000

    Update grabcut.py

--- grabcut.py
+++ grabcut.py
@@ -13,24 +13,29 @@ startXL = 40
 startYL = 50
 rect = (startXL, startYL, (320-startXL),(700-startYL))
 
-cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-leftlung = img*mask2[:,:,np.newaxis]
-
 startXR = 340
 startYR = 130
 rect2 = (startXR, startYR, (590-startXR),(715-startYR))
 
-cv2.grabCut(img,mask,rect2,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-rightlung = img*mask2[:,:,np.newaxis]
+# leftlung = img*mask2[:,:,np.newaxis]
+
+cv2.grabCut(img,mask,rect2,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+mask3 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+# rightlung = img*mask3[:,:,np.newaxis]
+
+lungs = img*mask2[:,:,np.newaxis] + img*mask3[:,:,np.newaxis]
 
 cv2.imshow('Original image', img)
-cv2.imshow('Right lung', rightlung)
-cv2.imshow('Left lung', leftlung)
+# cv2.imshow('Right lung', rightlung)
+# cv2.imshow('Left lung', leftlung)
+cv2.imshow('Lungs', lungs)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # plt.imshow(img)
 # plt.colorbar()
 # plt.show()
+
+
